f2f5c7aa51588aa0f612deaa8d061d239feb13a8
Stefan <70825104+StefanInfoTech@users.noreply.github.com>
2023-03-10 18:42:13 -0600

    Python code made legible pushed to thirdtest branch

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,19 @@
-for x in range(1,101):
-    if x % 3 == 0 and x % 5 == 0:
+#For loop that's range starts at 1 and ends at 100
+for num in range(1,101):
+
+# if statement that checks if looped number is divisible by 3 and 5 by dividing and comparing to zero
+    if num % 3 == 0 and num % 5 == 0:
         fbval = 'FizzBuzz'
-    elif x % 3 == 0:
+        
+# elif statement that checks if looped number is divisible by 3 by dividing and comparing to zero
+    elif num % 3 == 0:
         fbval = 'Fizz'
-    elif x % 5 == 0:
+
+# elif statement that checks if looped number is divisible by 5 by dividing and comparing to zero
+    elif num % 5 == 0:
         fbval = 'Buzz'
+
+# else prints off all other numbers that don't fit parameters
     else:
-        fbval = x
+        fbval = num
     print(fbval)
